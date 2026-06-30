99c672325bd9d51aa10ae95d1dd3e132e4a7c8c3
Sumit Paul <pauls1317@gmail.com>
2026-06-30 20:06:00 +0530

    IMP: add middleware for parser

--- apps/payment-service/src/main.ts
+++ apps/payment-service/src/main.ts
@@ -1,8 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { PaymentServiceModule } from './payment-service.module';
+import * as express from 'express';
 
 async function bootstrap() {
   const app = await NestFactory.create(PaymentServiceModule);
+
+  app.use(
+    express.json({
+      type: ['application/json', 'application/cloudevents+json'],
+    }),
+  );
+
   await app.listen(process.env.port ?? 3001);
 }
+
 bootstrap();
